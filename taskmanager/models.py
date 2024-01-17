6e7be71fbae173a8c475e4952beb30dfbcfb0a26
David Eriksson <the.dvudd@gmail.com>
2024-01-17 11:47:15 +0000

    asd

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -11,13 +11,14 @@ class Category(db.Model):
         # __repr__ to represent itself in the form of a string
         return self.category_name
 
+
 class Task(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
-    task_description = db.Column(db.text, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
-    due_date= db.Column(db.Date, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
